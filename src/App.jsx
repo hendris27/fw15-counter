63a1aeef3e6377ff5efcf83d05e9e3681d925649
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-09-09 20:20:42 +0700

    change color bg

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import React from 'react'
 import Counter from './components/Counter'
 
 function App() {
-  const [num, setNum] = React.useState(0)
- 
-  return (
-    <>
-      <div className="bg-green-500 flex flex-col h-screen justify-center items-center gap-4" >
-      <div className="bg-gray-700 w-[40px] h-[35px] flex justify-center items-center text-white rounded-[10px]">
-      {num}
-      </div>
-     <Counter onChangeNumber={setNum}/>
-      </div>
-    
-    </>
-  )
+	const [num, setNum] = React.useState(0)
+
+	return (
+		<>
+			<div className="bg-gray-100 flex flex-col h-screen justify-center items-center gap-4">
+				<div className="bg-gray-700 w-[40px] h-[35px] flex justify-center items-center text-white rounded-[10px]">{num}</div>
+				<Counter onChangeNumber={setNum} />
+			</div>
+		</>
+	)
 }
 
 export default App
