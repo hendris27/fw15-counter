337eacdc51cead3493714629fd8b9942291d3188
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-09-10 21:50:55 +0700

    fix linter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  daisyui:{
-    themes: [{
-      defaultTheme: {
-        primary:'#f8f9fa',
-        secondary:'red'
-      }
-    }]
-  },
-  plugins: [
-    require("daisyui")
-  ],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: [
+			{
+				defaultTheme: {
+					primary: '#f8f9fa',
+					secondary: 'red',
+				},
+			},
+		],
+	},
+	plugins: [require('daisyui')],
 }
-
